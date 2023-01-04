9e75f4d870b3c0d26af6033dc41ede1121426913
Spitzy <edipeev123@gmail.com>
2023-01-04 17:19:02 +0200

    project setup

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+      },
+  })
+
+  win.loadFile('index.html')
+}
+// In Electron, BrowserWindows can only be created after the app module's ready event is fired. 
+// You can wait for this event by using the app.whenReady() API and calling createWindow() once its promise is fulfilled.
+app.whenReady().then(() => {
+  createWindow()
+})
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
