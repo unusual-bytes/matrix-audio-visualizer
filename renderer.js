dbc00438bcc217e61a3e54a025e903e3824724e1
Spitzy <edipeev123@gmail.com>
2023-01-05 15:03:38 +0200

    Example button press handle

--- renderer.js
+++ renderer.js
@@ -1,5 +1,9 @@
 const {ipcRenderer} = require('electron')
-ipcRenderer.send('start-recording')
+
+// Handle the button press and then tell the backend (main process) to act on the button press
+document.getElementById('startRecordBTN').addEventListener('click', () =>{
+    ipcRenderer.send('start-recording')
+})
 
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${process.versions.chrome}), Node.js (v${process.versions.node}), and Electron (v${process.versions.electron})`
