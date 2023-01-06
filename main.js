f412c6aaefefe278cf6566a735218d26c85600df
Spitzy <edipeev123@gmail.com>
2023-01-06 12:54:51 +0200

    implement audio recording, ipc communication doesn't work

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, desktopCapturer, webContents } = require('electron')
 const path = require('path')
 const startAudioRecorder = require('./audio');
 
@@ -12,7 +12,22 @@ const createWindow = () => {
       },
   })
 
+  desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+    for (const source of sources) {
+      if (source.name === 'Entire Screen' || 'Screen 1') {
+        webContents.getAllWebContents().forEach(webContents => {
+          console.log("sendin")
+          webContents.send('SET_SOURCE', { message: '${source.id}'})
+        })
+        return
+      }
+    }
+})
+
   win.loadFile('index.html')
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.send('ping', 'whoooooooh!')
+  })
 }
 // In Electron, BrowserWindows can only be created after the app module's ready event is fired. 
 // You can wait for this event by using the app.whenReady() API and calling createWindow() once its promise is fulfilled.
--- renderer.js
+++ renderer.js
@@ -1,9 +1,48 @@
-const {ipcRenderer} = require('electron')
+const { ipcRenderer } = require('electron')
 
 // Handle the button press and then tell the backend (main process) to act on the button press
-document.getElementById('startRecordBTN').addEventListener('click', () =>{
-    ipcRenderer.send('start-recording')
-})
+// document.getElementById('startRecordBTN').addEventListener('click', () =>{
+//     ipcRenderer.send('start-recording')
+// })
+
+ipcRenderer.on('ping', (event, message) => {
+    console.log(message) // Prints 'whoooooooh!'
+  })
+
+ipcRenderer.on('SET_SOURCE', (event, sourceId) => {
+    console.log("received")
+    try {
+        console.log("tryin")
+      const stream = navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: sourceId.message,
+            minWidth: 1280,
+            maxWidth: 1280,
+            minHeight: 720,
+            maxHeight: 720
+          }
+        }
+      })
+      handleStream(stream)
+    } catch (e) {
+      handleError(e)
+    }
+  })
+  
+  function handleStream (stream) {
+    console.log("handleStream called")
+    const video = document.querySelector('video')
+    //video.src = URL.createObjectURL(stream)
+    video.srcObject = stream
+    video.onloadedmetadata = (e) => video.play()
+  }
+  
+  function handleError (e) {
+    console.log(e)
+  }
 
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${process.versions.chrome}), Node.js (v${process.versions.node}), and Electron (v${process.versions.electron})`
