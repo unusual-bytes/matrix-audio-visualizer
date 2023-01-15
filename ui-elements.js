04544dac1bcea09a0db6e8a8778b666ca62e6ebf
Spitzy <edipeev123@gmail.com>
2023-01-15 13:51:48 +0200

    Switch through divs, functional buttons

--- ui-elements.js
+++ ui-elements.js
@@ -1,7 +1,8 @@
 // TITLEBAR
 
-var exitButton = document.getElementById('exit-button');
-var minimizeButton = document.getElementById('minimize-button');
+// exit/minimize
+const exitButton = document.getElementById('exit-button');
+const minimizeButton = document.getElementById('minimize-button');
 
 exitButton.addEventListener('click', () => closeApp())
 minimizeButton.addEventListener('click', () => minimizeApp())
@@ -11,9 +12,56 @@ const minimizeApp = () => ipcRenderer.send('MINIMIZE-APP')
 
 // ----------------------------------------------------------------
 
+// menu buttons
+const settingsPageButton = document.getElementById('settings-page-button');
+const settingsPage = document.getElementById('settings-contents');
+
+const effectsPageButton = document.getElementById('effects-page-button');
+const effectsPage = document.getElementById('effects-contents');
+
+let settingsActive = true, effectsActive;
+
+settingsPageButton.addEventListener('click', () => openSettingsPage())
+effectsPageButton.addEventListener('click', () => openEffectsPage())
+
+settingsPageButton.addEventListener('mouseenter', () => enableHoverColor(settingsPageButton, settingsActive))
+settingsPageButton.addEventListener('mouseleave', () => disableHoverColor(settingsPageButton, settingsActive))
+effectsPageButton.addEventListener('mouseenter', () => enableHoverColor(effectsPageButton, effectsActive))
+effectsPageButton.addEventListener('mouseleave', () => disableHoverColor(effectsPageButton, effectsActive))
+
+const enableHoverColor = (buttonElement, buttonBool) => {
+  if(!buttonBool) buttonElement.style.backgroundColor = "#7743608a";
+}
+const disableHoverColor = (buttonElement, buttonBool) => {
+  if(!buttonBool) buttonElement.style.backgroundColor = "#4C3A51";
+}
+
+const openSettingsPage = () => {
+  settingsPageButton.style.backgroundColor = "#774360";
+  effectsPageButton.style.backgroundColor = "#4C3A51";
+
+  settingsPage.removeAttribute("hidden");
+  effectsPage.setAttribute("hidden", "");
+
+  settingsActive = true;
+  effectsActive = false;
+} 
+const openEffectsPage = () => {
+  settingsPageButton.style.backgroundColor = "#4C3A51";
+  effectsPageButton.style.backgroundColor = "#774360";
+
+  effectsPage.removeAttribute("hidden");
+  settingsPage.setAttribute("hidden", "");
+
+  settingsActive = false;
+  effectsActive = true;
+}
+
+// ----------------------------------------------------------------
+
 // DROPDOWN
 
-var dropdown = document.querySelector('.dropdown');
+const dropdown = document.querySelector('.dropdown');
 
 dropdown.addEventListener('click', function(event) {
   event.stopPropagation();
