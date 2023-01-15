4bd20b41ab5842453829662c97865ce1b72b5084
Spitzy <edipeev123@gmail.com>
2023-01-15 12:56:44 +0200

    UI changes 2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var win;
 const createWindow = () => {
   win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 550,
     frame: false,
     webPreferences: {
       nodeIntegration:true,
