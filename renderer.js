d904d9b2b2a1f4bf099c127b439a4f1f840d8c48
Spitzy <edipeev123@gmail.com>
2023-01-22 16:49:48 +0200

    Different audio visualization settings

--- renderer.js
+++ renderer.js
@@ -21,8 +21,10 @@ function handleStream(stream) {
   var audioCtx = new AudioContext();
   var src = audioCtx.createMediaStreamSource(stream);
   var analyser = audioCtx.createAnalyser();
+  analyser.minDecibels = -130;
+  analyser.maxDecibels = -10;
 
-  analyser.fftSize = 64;
+  analyser.fftSize = 2048;
   let frequencyArr = new Uint8Array(analyser.frequencyBinCount);
 
   src.connect(analyser);
@@ -33,7 +35,34 @@ function realtimeFrequencyData(frequencyArr, analyser) {
   // TODO: Fix: All frequencies under ~900Hz get grouped in the first few values. Elements like 808s and kicks as well as parts of the melody get missed and shown all in one group of pixels (just 3 pixels on the LED matrix).
   analyser.getByteFrequencyData(frequencyArr);
   visualizeDataToCanvas(frequencyArr);
-  ipcRenderer.send("SEND-SERIAL", frequencyArr);
+
+  const binnedArray = [];
+  const binSize = frequencyArr.length / 48;
+
+  // average value
+  // for (let i = 0; i < 32; i++) {
+  //   const start = i * binSize;
+  //   const end = start + binSize;
+  //   const bin = frequencyArr.slice(start, end);
+  //   const nonZeroValues = bin.filter((value) => value !== 0);
+  //   let binnedValue = 0;
+  //   if (nonZeroValues.length > 0) {
+  //     binnedValue =
+  //       nonZeroValues.reduce((a, b) => a + b) / nonZeroValues.length;
+  //   }
+  //   binnedArray.push(binnedValue);
+  // }
+
+  // max value
+  for (let i = 0; i < 32; i++) {
+    const start = i * binSize;
+    const end = start + binSize;
+    const bin = frequencyArr.slice(start, end);
+
+    binnedArray.push(Math.max.apply(Math, bin));
+  }
+
+  ipcRenderer.send("SEND-SERIAL", binnedArray);
 }
 
 function visualizeDataToCanvas(dataArr) {
